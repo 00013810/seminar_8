4bbc63d5a676fefe0a17288fab3ce9a4bc4ff3eb
00013810 <wiut00013810>
2022-11-04 09:36:30 +0500

    initial: commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# Task_1 Comparing two tuples
+
+melody = ('17', '00012456', 'Tashkent')
+veritas = ('19', '00015786', 'Canada')
+
+greater = melody > veritas
+greater_equal = melody >= veritas
+less_equal = melody <= veritas
+equal = melody == veritas
+not_equal = melody != veritas
+
+print(f'Result is equal: {greater}')
+print(f'Result is equal: {greater_equal}')
+print(f'Result is equal: {less_equal}')
+print(f'Result is equal: {equal}')
+print(f'Result is equal: {not_equal}')
+
+
+
+
+
+
+
+
